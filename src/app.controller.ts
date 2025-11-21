2036f71cb7dfb6d2bc907dd757c449932a0a4757
Juan Pablo <jppd0657@gmail.com>
2025-11-21 14:05:34 -0600

    feat: add @nestjs/config for environment variable management and update AppController to utilize config service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users/users.service';
+import { ConfigService } from '@nestjs/config';
+import { AppService } from './app.service';
+import { Env } from './env.model';
 
 @Controller()
 export class AppController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly usersService: UsersService,
+    private readonly configService: ConfigService<Env>,
+  ) {}
 
   @Get()
   getHello() {
     return this.usersService.findAll();
+  }
+
+  @Get('config')
+  getConfig() {
+    const myEnvVariable = this.configService.get('MY_ENV_VARIABLE', { infer: true });
+    const message = this.appService.getHello();
+    return {
+      message,
+      myEnvVariable,
+    };
   }
 }
---
+++ src/env.model.ts
@@ -0,0 +1,4 @@
+export interface Env {
+  MY_ENV_VARIABLE: string;
+  MY_OTHER_ENV_VARIABLE?: string;
+}
