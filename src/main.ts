e558ff2496e106b7127530e3a810a6bbb69296a1
Juan Pablo <jppd0657@gmail.com>
2025-11-27 17:11:19 -0600

    feat: add Swagger integration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,13 @@ async function bootstrap() {
       forbidUnknownValues: true,
     }),
   );
+
+  const config = new DocumentBuilder().setTitle('Blog API').setDescription('Blog API Description').setVersion('1.0').build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory, {
+    jsonDocumentUrl: 'swagger-json',
+    yamlDocumentUrl: 'swagger-yaml',
+  });
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   await app.listen(process.env.PORT ?? 3000);
